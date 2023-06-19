327afda81461549ed8811e0540babc12823088d8
Naveenpilania <naveenpilania847@gmail.com>
2023-06-19 14:30:30 +0530

    Innitial Source

--- madara/__init__.py
+++ madara/__init__.py
@@ -5,6 +5,6 @@ import time
 openai_key ="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"
 api_id = "1125689"
 api_hash = "4772d1792ed194020a8fb06a91ffb8fa"
-bot_token = "6267708263:AAFUp5DRNjJt4mkTKQXZFdZnTqm8MN12i24"
+bot_token = "5794992821:AAFUF1v7_g7ySdsdUz2Hh9jWR1czLWRWRc4"
 
-bot = TelegramClient(("virat_bot"), api_id, api_hash).start(bot_token = bot_token)
+bot = TelegramClient(("amit_bot"), api_id, api_hash).start(bot_token = bot_token)
--- madara/__main__.py
+++ madara/__main__.py
@@ -1,11 +1,11 @@
 import glob
 from pathlib import Path
-from madara.utils import load_plug
+from amit.utils import load_plug
 import logging
 from . import bot 
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
-path = "madara/plugins/*.py"
+path = "amit/plugins/*.py"
 files = glob.glob(path)
 for name in files:
   with open(name) as a:
@@ -13,7 +13,7 @@ for name in files:
     plugs = pxt.stem
     load_plug(plugs.replace(".py", ""))
     
-print ("VIRAT BOT HAS STARTED & LOADED ALL PLUGINS ")   
+print ("AMIT BOT HAS STARTED & LOADED ALL PLUGINS ")   
 
 if __name__ == "__main__":
   bot.run_until_disconnected()
